9c2f532f856aba5f41518f3b52f7a9e2a8edf36e
Mustafa Kemal Gordesli <m.kemalgordesli@gmail.com>
2022-08-02 16:20:47 +0300

    counter updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import "./App.scss";
 import Clock from "./components/clock/Clock.js";
 import Button from "./components/button/Button";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [time, SetTime] = useState({
+    hour: 0,
+    minute: 0,
+    second: 0,
+  });
+
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (isActive) {
+      interval = setInterval(() => {
+        let tempSec = time.second + 1;
+        let tempMin = time.minute + Math.floor(tempSec / 60);
+        let tempHour = time.hour + Math.floor(tempSec / 60);
+        if (tempSec >= 60) tempSec -= 60;
+        if (tempMin >= 60) tempMin -= 60;
+        SetTime({
+          hour: tempHour,
+          minute: tempMin,
+          second: tempSec,
+        });
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  });
+
   const [shows, SetShows] = useState({
     start: true,
     pause: false,
@@ -11,6 +38,7 @@ function App() {
   });
 
   function handleStart() {
+    setIsActive(true);
     SetShows({
       start: false,
       pause: true,
@@ -19,6 +47,7 @@ function App() {
   }
 
   function handlePause() {
+    setIsActive(false);
     SetShows({
       start: false,
       pause: false,
@@ -27,6 +56,7 @@ function App() {
   }
 
   function handleContinue() {
+    setIsActive(true);
     SetShows({
       start: false,
       pause: true,
@@ -35,6 +65,7 @@ function App() {
   }
 
   function handleClear() {
+    setIsActive(false);
     SetShows({
       start: true,
       pause: false,
@@ -52,7 +83,7 @@ function App() {
         </div>
         <div className="row">
           <div className="col-12">
-            <Clock />
+            <Clock time={time} />
           </div>
         </div>
         <div className="row mt-2">
--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -1,23 +1,17 @@
 import "./Clock.scss";
-import React, { useState } from "react";
 
-function Clock() {
-  const [minute, setMinute] = useState("00");
-  const [hour, setHour] = useState("00");
-  const [second, setSecond] = useState("00");
-  const [interval, setInterval] = useState(0);
-
+function Clock(props) {
   return (
     <div className="Clock">
       <div className="Clock-container">
         <div className="Clock-container-items">
-          <p id="hour">{hour}</p>
+          <p id="hour">{props.time.hour}</p>
         </div>
         <div className="Clock-container-items">
-          <p id="minute">{minute}</p>
+          <p id="minute">{props.time.minute}</p>
         </div>
         <div className="Clock-container-items">
-          <p id="second">{second}</p>
+          <p id="second">{props.time.second}</p>
         </div>
       </div>
     </div>
